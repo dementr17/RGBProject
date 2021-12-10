935290441072c037c5983eda40da7233cc367bb4
Dmitry <ecawd>
2021-12-10 10:06:23 +0300

    Ready App

--- RGBProject/ViewController.swift
+++ RGBProject/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet var rgbView: UIView!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    private var redColor: CGFloat = 0.5
+    private var greenColor: CGFloat = 0.5
+    private var blueColor: CGFloat = 0.5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        redSlider.value = 0.5
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redSlider.minimumTrackTintColor = .red
+        
+        greenSlider.value = 0.5
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        greenSlider.minimumTrackTintColor = .green
+        
+        blueSlider.value = 0.5
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        blueSlider.minimumTrackTintColor = .blue
+        //redSlider.value
+        //rgbView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+        //rgbView.backgroundColor = UIColor(red: 0.5, green: 0.5, blue: 1, alpha: 1)
+        
+        
+
+    }
+    @IBAction func choseRed(_ sender: Any) {
+        redColor = CGFloat(redSlider.value)
+        redLabel.text = String(format:"%.2f", redSlider.value)
+        rgbView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+    }
+    @IBAction func choseGreen(_ sender: Any) {
+        greenColor = CGFloat(greenSlider.value)
+        greenLabel.text = String(format:"%.2f", greenSlider.value)
+        rgbView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+    }
+    @IBAction func choseBlue(_ sender: Any) {
+        blueColor = CGFloat(blueSlider.value)
+        blueLabel.text = String(format:"%.2f", blueSlider.value)
+        rgbView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+    }
+    
 
 }
 
