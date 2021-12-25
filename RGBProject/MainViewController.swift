e6e1d0bd7c90acb7ce193e33d4da5d16f192de81
Dmitry <ecawd>
2021-12-25 16:07:19 +0300

    v.3.0

---
+++ RGBProject/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  RGBProject
+//
+//  Created by Дмитрий Чепанов on 25.12.2021.
+//
+
+import UIKit
+
+class MainViewController {
+    
+    func viewDidLoad() {
+        
+    }
+}
--- RGBProject/SettingsViewController.swift
+++ RGBProject/SettingsViewController.swift
@@ -7,15 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SettingsViewController: UIViewController {
     
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+    
     @IBOutlet var rgbView: UIView!
+    
     @IBOutlet weak var redLabel: UILabel!
     @IBOutlet weak var greenLabel: UILabel!
     @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redTF: UITextField!
+    @IBOutlet weak var greenTF: UITextField!
+    @IBOutlet weak var blueTF: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,21 +29,33 @@ class ViewController: UIViewController {
         rgbView.layer.cornerRadius = 10
         rgbView.backgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1)
         
-        redSlider.minimumTrackTintColor = .red
-        redLabel.text = String(format:"%.2f", redSlider.value)
+        backgroundViewColor()
         
-        greenSlider.minimumTrackTintColor = .green
-        greenLabel.text = String(format:"%.2f", greenSlider.value)
         
-        blueSlider.minimumTrackTintColor = .blue
-        blueLabel.text = String(format:"%.2f", blueSlider.value)
+        
 
     }
     
     @IBAction func chooseRGB() {
+        backgroundViewColor()
+    }
+}
+
+extension SettingsViewController {
+    
+    private func backgroundViewColor() {
         redLabel.text = String(format:"%.2f", redSlider.value)
         greenLabel.text = String(format:"%.2f", greenSlider.value)
         blueLabel.text = String(format:"%.2f", blueSlider.value)
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
+        redTF.text = String(format:"%.2f", redSlider.value)
+        greenTF.text = String(format:"%.2f", greenSlider.value)
+        blueTF.text = String(format:"%.2f", blueSlider.value)
+        
         rgbView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
                                           green: CGFloat(greenSlider.value),
                                           blue: CGFloat(blueSlider.value),
